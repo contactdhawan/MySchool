b6c7ab27783dc96ec1bc26e85494bbe83338c1dc
neeraj <contactdhawan@gmail.com>
2017-01-14 23:33:51 -0600

    annotation

--- src/main/java/sf/ecrm/controllers/WelcomeController.java
+++ src/main/java/sf/ecrm/controllers/WelcomeController.java
@@ -3,14 +3,16 @@ package sf.ecrm.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class WelcomeController extends AbstractController {
+@Controller
+public class WelcomeController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest req,
-			HttpServletResponse res) throws Exception {
+@RequestMapping("/welcome")
+	protected ModelAndView handleRequestInternal() {
 		System.out.println("In the controller");
 		ModelAndView mav = new ModelAndView("Welcome");
 		mav.addObject("welcomeMessage", "Welcome to Dhawan Family");
